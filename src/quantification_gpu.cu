40127428b275712e1c0dd0383258ca66c75fcff4
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-01-03 14:10:39 +0100

    feat: Added verify result function and finished quantify_gpu function

--- src/quantification_gpu.cu
+++ src/quantification_gpu.cu
@@ -19,21 +19,76 @@ __global__ void quantify_kernel(int16_t *array, bool luminance)
     }
 }
 
-int16_t *quantify_cuda(int16_t *array, bool luminance)
+void quantify_gpu(int16_t *array, bool luminance)
 {
-    // Définissez les dimensions du bloc et du grille
+    // Give size to allocate on GPU
+    const int size = 64*sizeof(int16_t);
+
+    // Allocate memory on the device
+    int16_t *d_array;
+    bool d_luminance;
+    cudaMalloc(&d_array, size);
+    cudaMalloc(&d_luminance, sizeof(bool));
+
+    // Copy data from the host to the device (CPU -> GPU)
+    cudaMemcpy(d_array, h_array, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_luminance, luminance, sizeof(bool), cudaMemcpyHostToDevice);
+
+    // TODO
+    // Define block size and grid size
     const dim3 block_size(32, 8, 1);
     const dim3 grid_size(ceil(64 / block_size.x), 8, 1);
 
-    // Initialisez le tableau de données quantifié
-    int16_t *quantified_array = (int16_t *)malloc(64 * sizeof(int16_t));
+    // Execute kernel
+    quantify_kernel<<<grid_size, block_size>>>(d_array, d_luminance);
 
-    // Appelez la fonction CUDA
-    quantify_kernel<<<grid_size, block_size>>>(quantified_array, luminance);
-
-    // Attendez la fin de l'exécution de la fonction CUDA
+    // Wait for kernel completion
     cudaDeviceSynchronize();
 
-    // Renvoyez le tableau de données quantifié
-    return quantified_array;
+    // Copy result of computation back on host
+    cudaMemcpy(h_array, d_array, size, cudaMemcpyDeviceToHost);
+}
+
+void verify_result(int16_t *array_cpu, int16_t *array_gpu)
+{
+    for (int i = 0; i < 64; ++i) {
+        assert(array_cpu[i] == array_gpu);
+    }
+}
+
+/*
+    Quantifier le bloc 8x8 (qui a été transformé en array zigzag)
+    Entrées : array int16_t de taille 64 et un bool qui vérifier si on est dans le cas Y ou CbCr
+    Sortie : Rien 
+*/
+void quantify_cpu(int16_t *array, bool luminance)
+{
+    /*
+        diviser terme à terme chaque bloc 8x8 par une matrice de quantification (sous le forme zig-zag déjà) suivant Y ou CbCr
+    */
+    for (uint8_t i=0; i<64; i++){
+        if (luminance){
+            array[i] = (array[i] / quantification_table_Y[i]);
+        }
+        else{
+            array[i] = (array[i] / quantification_table_CbCr[i]);
+        }
+    }
+}
+
+void quantify(int16_t *array, bool luminance)
+{
+    int16_t *array_copy = malloc(64*sizeof(int16_t));
+    // Create a copy of the original array
+    for (int i = 0; i < 64; ++i) {
+        array_copy[i] = array[i]
+    }
+    
+    // Run this on CPU
+    quantify_cpu(array, luminance);
+
+    // Run this on GPU
+    quantify_gpu(array_copy, luminance);
+
+    verify_result(array, array_copy);
 }
