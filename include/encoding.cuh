39957f3c8a128578a68c77fbb64220598c7c274b
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-02-11 22:51:42 +0100

    Added a wrapper in the header when compiling with C++ compiler

--- include/encoding.cuh
+++ include/encoding.cuh
@@ -1,3 +1,11 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
-extern void encoding(int16_t *mcus_line_array, uint32_t mcus_line_array_size, bool luminance);
+void encoding(int16_t *mcus_line_array, uint32_t mcus_line_array_size, bool luminance);
+
+#ifdef __cplusplus
+}
+#endif
