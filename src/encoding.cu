73679fd713760ed4b7e7c579747032d2f952804f
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-02-12 01:00:07 +0100

    Added debug print statement

--- src/encoding.cu
+++ src/encoding.cu
@@ -78,6 +78,7 @@ __global__ void encoding_gpu(int16_t *mcus_line_array, uint32_t nb_mcu_line, uin
     uint32_t tx = threadIdx.x;
     uint32_t ty = threadIdx.y;
     uint32_t block_offset = blockIdx.x * blockDim.x * blockDim.y;
+    printf("Dim x = %i, Dim y = %i\n", blockDim.x, blockDim.y);
 
     // check if within bounds
     // if (x < (nb_mcu_line - 1) * 8 + 8 && y < (nb_mcu_line - 1) * 8 + 8) {
