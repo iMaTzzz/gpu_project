535c45e1a48e8a12523be8e5f28bdd5733ea7cc3
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-02-13 15:40:10 +0100

    Added print statement for debugging

--- src/coding.c
+++ src/coding.c
@@ -126,7 +126,6 @@ void coding_mcus_line(int16_t *mcus_line_array, uint32_t nb_mcus_line, struct hu
                       struct bitstream *stream, int16_t *predicator, uint16_t *index)
 {
     for (uint32_t mcu_index = 0; mcu_index < nb_mcus_line; ++mcu_index) {
-        printf("Mcu index = %u\n", mcu_index);
         uint64_t offset = 64 * mcu_index; 
         /* On encode d'abord le coefficient DC */
         uint8_t magnitude_dc = magnitude(mcus_line_array[offset + 0] - *predicator);
