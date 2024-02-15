0aba90976871de7fecf8cce4126436ab3555c7cb
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-02-15 11:49:33 +0100

    Commented print statements and fixed rgb to YCbCr function

--- src/rgb_to_ycbcr.c
+++ src/rgb_to_ycbcr.c
@@ -14,24 +14,15 @@
 */
 uint8_t rgb_to_ycbcr(uint8_t red, uint8_t green, uint8_t blue, enum color_component cc)
 {
-    if (cc == Y) {
-        for (uint8_t i = 0; i < 8; i++) {
-            for (uint8_t j = 0; j < 8; j++) {
-                return (uint8_t)(0.299 * red + 0.587 * green + 0.114 * blue);
-            }
-        }
-    } else if (cc == Cb) {
-        for (uint8_t i = 0; i < 8; i++) {
-            for (uint8_t j = 0; j < 8; j++) {
-                return (uint8_t)(-0.1687 * red - 0.3313 * green + 0.5 * blue + 128);
-            }
-        }
-    } else {
-        for (uint8_t i = 0; i < 8; i++) {
-            for (uint8_t j = 0; j < 8; j++) {
-                return (uint8_t)(0.5 * red - 0.4187 * green - 0.0813 * blue + 128);
-            }
-        }
+    switch (cc) {
+        case Y:
+            return (uint8_t)(0.299 * red + 0.587 * green + 0.114 * blue);
+        case Cb:
+            return (uint8_t)(-0.1687 * red - 0.3313 * green + 0.5 * blue + 128);
+        case Cr:
+            return (uint8_t)(0.5 * red - 0.4187 * green - 0.0813 * blue + 128);
+        default:
+            break;
     }
-    return 0;
+    exit(EXIT_FAILURE);
 }
