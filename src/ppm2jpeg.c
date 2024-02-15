a56f79e1dd54648adb60439f8dedb7249ef957f7
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-02-15 16:10:40 +0100

    Added print statement

--- src/ppm2jpeg.c
+++ src/ppm2jpeg.c
@@ -235,7 +235,7 @@ static void start_test(char* dir_path, uint8_t h1, uint8_t v1, uint8_t h2, uint8
         printf("nb_images: %u\n", nb_images);
         nb_images += 1;
         printf("nb_images: %u\n", nb_images);
-        if (nb_images == 2) {
+        if (nb_images == 3) {
             return;
         }
     }
